e598922393be7bb26ba754cc8e5566accb4650e3
Lucas Sotelo <lucasnsotelog@gmail.com>
2018-11-28 20:13:37 -0300

    Added plugin installation

--- install.sh
+++ install.sh
@@ -13,4 +13,20 @@ ln -sf ~/.dotfiles/.gitconfig ~/
 ln -sf ~/.dotfiles/.vimrc ~/
 ln -sf ~/.dotfiles/.bashrc ~/
 ln -sf ~/.dotfiles/.tmux.conf ~/
+# Instalo los plugins de vim
+echo "Instalando plugins..."
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+echo "Instalado pathogen"
+cd ~/.vim/bundle
+git clone https://github.com/morhetz/gruvbox
+echo "Instalado gruvbox"
+git clone https://github.com/vim-airline/vim-airline
+echo "Instalado vim-airline"
+git clone https://github.com/edkolev/tmuxline.vim
+echo "Instalado tmuxline"
+git clone https://github.com/wincent/command-t
+echo "Instalado command-t"
+git clone https://github.com/scrooloose/nerdtree
+echo "Instalado NERDTree"
 echo "Listo!"
